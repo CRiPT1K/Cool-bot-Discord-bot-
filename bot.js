3e431fead597eada3c61367ac105554880a2ec6c
Airrocket <32821049+Airrocket@users.noreply.github.com>
2017-10-15 14:46:05 -0600

    Updated copy pasta

--- bot.js
+++ bot.js
@@ -13,7 +13,7 @@ var bot = new Discord.Client({
    autorun: true
 });
 var memeList = ['When life gives you lemons make orange juice', 'lol memes', 'r/dank memes',
-		'the memes are currently offline, please insert a quarter to continue'];
+		'the memes are currently offline, please insert a quarter to continue', 'https://youtu.be/eXBqf4Gfuo0'];
 var pastaList = ['(Weird Roblox Desc.) Hello. My life on roblox is all about fun. Im here for my bestfriend.Tiana Shes a diamond. Her name suits her cause shes a princess Shes my life No one can seperate us. Yes we do fight. But it only means we are true bestfriends. I dont care how much you try to seperate us and steal her from me. Beacause ill always protect her. Shes not in the top t e n. Shes in the top o ne :). Shes such a good roleplayer. She understands me. She protects me. She cares about me like I do. Were twins even doe were not the same Thats what we would say. You can do what you want whit your little seperating plans. But it will never work whit me and Tia. I ♥ you Tia :)']
 logger.info (memeList)
 bot.on('ready', function (evt) {
@@ -65,7 +65,7 @@ bot.on('message', function (user, userID, channelID, message, evt) {
             break
             
 	    case 'copyPasta':
-            	var pastaNum = Math.floor(Math.random()*memeList.length)
+            	var pastaNum = Math.floor(Math.random()*pastaList.length)
 	    		bot.sendMessage({
             		to: channelID,
                     	message: pastaList[pastaNum]
